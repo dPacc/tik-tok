660e785da4c36f40aa254119053ad2a7eaed2a01
dPacc <themdk01@gmail.com>
2020-11-01 19:04:32 +0400

    added pause play with ref, snap, styled the video component

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./Video.css";
 import myVideo from "../../utils/videos/tiktokvideo_1604132845.mp4";
 
 export default function Video() {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  const handleVideoPress = () => {
+    // If video is playing, stop ut
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    }
+    // If video is stopped, play it
+    else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
+
   return (
     <div className="video">
-      <video loop className="video__player" src={myVideo}></video>
+      <video
+        onClick={handleVideoPress}
+        loop
+        ref={videoRef}
+        className="video__player"
+        src={myVideo}
+      ></video>
 
       {/* Video Footer */}
       {/* Video Sidebar */}
